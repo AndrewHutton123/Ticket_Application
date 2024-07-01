dbd6f801962f627e47fc76a4d392d81a37fcd589
AndrewHutton123 <andrewhu11on123@gmail.com>
2024-07-01 21:13:45 +0100

    padgination added to ticket table

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,9 +3,22 @@ import prisma from "@/prisma/db";
 import DataTable from "./DataTable";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import Pagination from "@/components/Pagination";
+import { Ticket } from "@prisma/client";
 
-const Tickets = async () => {
-  const tickets: any = await prisma.ticket.findMany();
+type SearchParams = {
+  page: string;
+};
+
+const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+  const ticketCount = await prisma.ticket.count();
+
+  const tickets: Ticket[] = await prisma.ticket.findMany({
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
 
   return (
     <div>
@@ -16,6 +29,11 @@ const Tickets = async () => {
         Create New Ticket
       </Link>
       <DataTable tickets={tickets} />
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
