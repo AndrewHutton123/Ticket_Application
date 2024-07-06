3be7921dc49067793db2cd8cedb0490b9209649b
AndrewHutton123 <andrewhu11on123@gmail.com>
2024-07-06 08:45:43 +0100

    Created a user table and allowed updating users

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -24,10 +24,13 @@ export async function PATCH(request: NextRequest, { params }: Props) {
     return NextResponse.json({ error: "User not Found" }, { status: 404 });
   }
 
-  if (body?.password) {
+  if (body?.password && body.password !== "") {
     const hashPassword = await bcrypt.hash(body.password, 10);
     body.password = hashPassword;
   }
+  //  else {
+  //   delete body.password;
+  // }
 
   if (user.username !== body.username) {
     const duplicateUsername = await prisma.user.findUnique({
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,14 @@
 import UserForm from "@/components/UserForm";
 import React from "react";
+import DataTableSimple from "./DataTableSimple";
+import prisma from "@/prisma/db";
 
-const Users = () => {
+const Users = async () => {
+  const users = await prisma.user.findMany();
   return (
     <div>
       <UserForm />
+      <DataTableSimple users={users} />
     </div>
   );
 };
